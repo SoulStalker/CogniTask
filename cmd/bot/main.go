afc5e1fd11152637d5fd0514b73385db269c7c31
Almaz Galimov <almazwork@gmail.com>
2025-06-16 18:14:20 +0500

    add useCase

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,25 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"time"
 
+	"github.com/SoulStalker/cognitask/internal/domain"
 	"github.com/SoulStalker/cognitask/internal/infra"
+	"github.com/SoulStalker/cognitask/internal/usecase"
 	"gorm.io/gorm/logger"
 )
 
 func main() {
 	// init storage
 	dbConfig := infra.DBConfig{DSN: "bot.db", Logger: logger.Default}
-	_, err := infra.InitDB(dbConfig)
+	db, err := infra.InitDB(dbConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 
 	// init repo
+	taskRepo := infra.New(db)
 
 	// init service
+	taskService := usecase.NewTaskService(taskRepo)
 
+	newTask, err := taskService.Add(domain.Task{
+		Description: "Do something",
+		Deadline: time.Now().AddDate(0, 0, 2),
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(newTask)
 	// init handler
 
 	// run bot polling
--- internal/usecase/task.go
+++ internal/usecase/task.go
@@ -1,3 +1,49 @@
 package usecase
 
-// бизнес-логика, типа "сохрани задачу", "удали задачу"
+import (
+	"time"
+
+	"github.com/SoulStalker/cognitask/internal/domain"
+)
+
+type TaskService struct {
+	repo domain.TaskRepository
+}
+
+func NewTaskService(repo domain.TaskRepository) *TaskService {
+	return &TaskService{
+		repo: repo,
+	}
+}
+
+func (s *TaskService) Add(task domain.Task) (domain.Task, error) {
+	return s.repo.Add(task)
+}
+
+func (s *TaskService) MarkDone(id uint) (bool, error) {
+	return s.repo.MarkDone(id)
+}
+
+func (s *TaskService) GetPending() ([]domain.Task, error) {
+	return s.repo.GetPending()
+}
+
+func (s *TaskService) EditDate(id uint, newDate time.Time) (domain.Task, error) {
+	return s.repo.EditDate(id, newDate)
+}
+
+func (s *TaskService) Delete(id uint) error {
+	return s.repo.Delete(id)
+}
+
+func (s *TaskService) GetExpired(deadline time.Time) ([]domain.Task, error) {
+	return s.repo.GetExpired(deadline)
+}
+
+func (s *TaskService) DeleteOldDone(N_days int) (int64, error) {
+	return s.repo.DeleteOldDone(N_days)
+}
+
+func (s *TaskService) GetByID(id uint) (domain.Task, error) {
+	return s.repo.GetByID(id)
+}

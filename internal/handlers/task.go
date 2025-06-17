a74950012f4ff607ed0328676dbb4d58f424eda3
Almaz Galimov <almazwork@gmail.com>
2025-06-17 18:06:27 +0500

    add middleware

--- internal/handlers/task.go
+++ internal/handlers/task.go
@@ -1,3 +1,8 @@
 package handlers
 
+import "gopkg.in/telebot.v3"
+
 // сюда пишем код, который обрабатывает команды /add, /list и т.д.
+func startHandler(c telebot.Context) error {
+	return c.Send("Привет! Я твой таск-менеджер")
+}
---
+++ internal/middleware/auth.go
@@ -0,0 +1,15 @@
+package middleware
+
+import "gopkg.in/telebot.v3"
+
+// Middleware проверяет chat_id перед каждым хендлером
+func authMiddleware(allowedChatID int64) telebot.MiddlewareFunc {
+	return func(next telebot.HandlerFunc) telebot.HandlerFunc {
+		return func(c telebot.Context) error {
+			if c.Sender().ID != allowedChatID {
+				return nil 
+			}
+			return next(c) 
+		}
+	}
+}

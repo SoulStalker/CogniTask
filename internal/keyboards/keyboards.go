7c8ad193930df9535a258e7425df133fcc7a02bd
Almaz Galimov <almazwork@gmail.com>
2025-06-18 15:06:35 +0500

    add other kbs

--- internal/keyboards/keyboards.go
+++ internal/keyboards/keyboards.go
@@ -1,6 +1,11 @@
 package keyboards
 
-import tele "gopkg.in/telebot.v3"
+import (
+	"fmt"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
 
 const (
 	// Кнопки выбора даты
@@ -15,6 +20,7 @@ const (
 	BtnRandomPic = "🎲 Random Pic"
 )
 
+// GetDateSelectionKeyboard клавиатура для выбора даты задачи
 func GetDateSelectionKeyboard() *tele.ReplyMarkup {
 	kb := &tele.ReplyMarkup{
 		ResizeKeyboard:  true,
@@ -36,4 +42,56 @@ func GetDateSelectionKeyboard() *tele.ReplyMarkup {
 	)
 
 	return kb
+}
+
+// GetTaskControlKeyboard клавиатура для управления задачей
+func GetTaskControlKeyboard(taskID uint) *tele.ReplyMarkup {
+	kb := &tele.ReplyMarkup{}
+
+	// Inline кнопки с callback data
+	btnComplete := kb.Data(BtnComplete, "complete", fmt.Sprintf("%d", taskID))
+	btnRandomPic := kb.Data(BtnRandomPic, "random_pic", "")
+
+	// Раскладка
+	kb.Inline(
+		kb.Row(btnComplete),
+		kb.Row(btnRandomPic), // todo или сюда лучше отмену поставить?
+	)
+
+	return kb
+}
+
+// GetRemoveKeyboard удаляет reply клаву
+func GetRemoveKeyboard() *tele.ReplyMarkup {
+	return &tele.ReplyMarkup{
+		RemoveKeyboard: true,
+	}
+}
+
+func GetTodayDate() string {
+	return time.Now().Format("2006-01-02")
+}
+
+func GetTomorrowDate() string {
+	return time.Now().AddDate(0, 0, 1).Format("2006-01-02")
+}
+
+// todo надо подумать может лучше просто принимать цифру текущего месяца
+// Парсинг даты из строки (для календаря)
+func ParseDate(dateStr string) (time.Time, error) {
+	// Поддерживаемые форматы
+	formats := []string{
+		"2006-01-02", // 2024-12-25
+		"02.01.2006", // 25.12.2024
+		"02/01/2006", // 25/12/2024
+		"02-01-2006", // 25-12-2024
+	}
+
+	for _, format := range formats {
+		if date, err := time.Parse(format, dateStr); err == nil {
+			return date, nil
+		}
+	}
+
+	return time.Time{}, fmt.Errorf("неподдерживаемый формат даты. Используйте: DD.MM.YYYY или YYYY-MM-DD")
 }

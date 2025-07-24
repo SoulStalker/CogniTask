429260ab809901308239a99cd195d50b9b6d2239
Almaz Galimov <almazwork@gmail.com>
2025-07-24 17:43:11 +0500

    pretty dates

--- internal/mappers/mappers.go
+++ internal/mappers/mappers.go
@@ -2,12 +2,28 @@ package mappers
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/SoulStalker/cognitask/internal/domain"
 	"github.com/SoulStalker/cognitask/internal/keyboards"
 	"github.com/SoulStalker/cognitask/internal/messages"
 	tele "gopkg.in/telebot.v3"
 )
+
+var russianShortMonths = map[time.Month]string{
+	time.January:   "янв.",
+	time.February:  "фев.",
+	time.March:     "мар.", // Обратите внимание, для короткой формы иногда используются другие склонения или формы
+	time.April:     "апр.",
+	time.May:       "мая",  // Короткая форма для мая часто совпадает с полной
+	time.June:      "июня", // Аналогично
+	time.July:      "июля", // Аналогично
+	time.August:    "авг.",
+	time.September: "сен.",
+	time.October:   "окт.",
+	time.November:  "ноя.",
+	time.December:  "дек.",
+}
 
 func FormatTaskList(tasks []domain.Task) [][]tele.InlineButton {
 	var rows [][]tele.InlineButton
@@ -17,11 +33,20 @@ func FormatTaskList(tasks []domain.Task) [][]tele.InlineButton {
 			Unique: keyboards.BtnComplete.Unique,
 			Data:   fmt.Sprint(task.ID),
 		}
+		var taskDate string
+		if !task.Deadline.IsZero() {
+			t := task.Deadline
+			day := t.Day()
+			monthName := russianShortMonths[t.Month()]
+
+			// taskDate = task.Deadline.Format("02-01")
+			taskDate = fmt.Sprintf("%d %s", day, monthName)
+		}
 		if !task.Closed {
-			btn.Text = fmt.Sprintf("✅ %s : %s", task.Description, task.Deadline.Format("02-01"))
+			btn.Text = fmt.Sprintf("✅ %s : %s", task.Description, taskDate)
 		} else {
 			btn.Unique = keyboards.BtnDelete.Unique
-			btn.Text = fmt.Sprintf("❌ %s : %s", task.Description, task.ClosedAt.Format("02-01"))
+			btn.Text = fmt.Sprintf("❌ %s : %s", task.Description, taskDate)
 		}
 		rows = append(rows, []tele.InlineButton{btn})
 	}
